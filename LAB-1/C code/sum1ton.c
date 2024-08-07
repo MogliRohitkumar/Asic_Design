d0bebba634efe3eaee5cc4ef627f8da2c77a7d17
Mogli Rohit Kumar <98601073+MogliRohitkumar@users.noreply.github.com>
2024-08-07 13:48:35 +0530

    Update sum1ton.c

--- LAB-1/C code/sum1ton.c
+++ LAB-1/C code/sum1ton.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+int main() {
+    int i, sum = 0, n=69;
+    for(int i = 1; i <= n; ++i)
+    {
+        sum += i;
+    }
+
+    printf("The sum of numbers from 1 to %d = %d\n", n, sum);
+    return 0;
+}
